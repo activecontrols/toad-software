cda32624fdb0dbf837be5bb4d33ed9f6de50e268
jacobh460 <jacobhillman016@gmail.com>
2026-08-27 19:36:41 -0400

    send function for sending a message in the bus (blocks until message is sent for simplicity); receive function for receiving a pending message from the receive fifo

--- firmware/lib/fdcan/fdcan_toad.h
+++ firmware/lib/fdcan/fdcan_toad.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <stdint.h>
+#include "stm32h7xx_hal.h"
 
-// todo - confirm which can bus the actuators are on
-namespace CAN1
+class CAN
 {
-    // initialize the FDCAN1 peripheral for use with the Toad EC
+public:
+    CAN(FDCAN_GlobalTypeDef* instance);
+
+    // initialize the FDCAN peripheral for use with the Toad EC
     void init(void);
 
     // get number of elements in the receive fifo
@@ -15,6 +18,14 @@ namespace CAN1
 
     // transmit a data frame in classic CAN mode with an 11 bit ID
     // return true on success, false on error
-    bool send(uint16_t id, )
-    
+    bool send(uint16_t id, uint32_t data_length, const uint8_t* data);
+
+
+    // if a message is waiting in the recieve FIFO, this returns true; otherwise returns false
+    // returns false if an error occurs
+    // if a message is waiting, and no error occurs, header and data are populated with the corresponding information regarding the received message, and the message is from the receive header
+    bool receive(FDCAN_RxHeaderTypeDef* header, uint8_t* data);
+
+private:
+    FDCAN_HandleTypeDef hfdcan;
 };
