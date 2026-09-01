84485de6f01de65a5c183957cd5d083465073858
jacobh460 <jacobhillman016@gmail.com>
2026-09-01 14:47:54 -0400

    modify valve_profile.py to support quitting in the middle of a profile (via ctrl+c), add requirements.txt for installing the required python libraries

--- scripts/valve_profile.py
+++ scripts/valve_profile.py
@@ -78,45 +78,47 @@ profile = profile_chirp
 
 
 def main():
-    # Ensure destination directory exists
-    log_dir = "./dry_tests"
-    os.makedirs(log_dir, exist_ok=True)
+    try:
+        # Ensure destination directory exists
+        log_dir = "./dry_tests"
+        os.makedirs(log_dir, exist_ok=True)
 
-    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=57600, timeout=1) 
+        ser = serial.Serial(port='/dev/ttyUSB0', baudrate=57600, timeout=1) 
 
-    out_path = os.path.join(log_dir, "out_chirp.csv")
-    fout = open(out_path, "w")
+        out_path = os.path.join(log_dir, "out_chirp.csv")
+        fout = open(out_path, "w")
 
-    ser.write(b"motor_follow_profile\n")
+        ser.write(b"motor_follow_profile\n")
 
-    # write the first line (the csv header)
-    fout.write(ser.readline().decode("utf-8"))
+        # write the first line (the csv header)
+        fout.write(ser.readline().decode("utf-8"))
 
-    time_s = 0.0
+        time_s = 0.0
 
-    while True:
-        do_quit, new_target = profile(time_s)
+        while True:
+            do_quit, new_target = profile(time_s)
 
-        if do_quit:
-            break
+            if do_quit:
+                break
 
-        ser.write((f"g {new_target:.2f}\n").encode("utf-8"))
+            ser.write((f"g {new_target:.2f}\n").encode("utf-8"))
 
-        # wait for data to come back
-        data_response = ser.readline().decode("utf-8")
+            # wait for data to come back
+            data_response = ser.readline().decode("utf-8")
 
-        # parse the time field
-        time_s = float(data_response.split(',')[0])
+            # parse the time field
+            time_s = float(data_response.split(',')[0])
 
-        # pipe the csv data to output file
-        fout.write(data_response)
+            # pipe the csv data to output file
+            fout.write(data_response)
 
-    # send quit command        
-    ser.write(b'q\n')
+    finally:
+        # send quit command        
+        ser.write(b'q\n')
 
-    # close handles
-    fout.close()
-    ser.close()
+        # close handles
+        fout.close()
+        ser.close()
     return
 
 
