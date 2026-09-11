d8a058080691574ff8ac7a317fc439bca4093f03
jacobh460 <jacobhillman016@gmail.com>
2026-09-11 17:28:39 -0400

    make the linker error go away that is caused by missing _write definition (was only occurring when building for fc; maybe there was a change to the Uart class since the previous stm32duino version that is causing us to see the error now?)

--- firmware/src/fc_main.cpp
+++ firmware/src/fc_main.cpp
@@ -1,6 +1,11 @@
-#include "CommsSerial.h"
+// #include "CommsSerial.h"
 #include <Arduino.h>
 
-void setup() {}
+void setup() 
+{
+    Serial.begin(9600);
+
+    Serial.println("init"); // had to add this; for some reason without it we don't pull in Print.cpp which causes a linker error because _write is not defined
+}
 
 void loop() {}
