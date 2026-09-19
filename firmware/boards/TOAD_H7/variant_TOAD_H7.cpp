58e27e2e57136d346656348631f6cfe2be27c9e5
jacobh460 <jacobhillman016@gmail.com>
2026-09-19 14:29:10 -0400

    pull in clock config from stm32cubemx, not sure if everything here is set up properly for usb or not. clock is set to 48MHz, is there other init we need to do?

--- firmware/boards/TOAD_H7/variant_TOAD_H7.cpp
+++ firmware/boards/TOAD_H7/variant_TOAD_H7.cpp
@@ -193,6 +193,78 @@ const uint32_t analogInputPin[] = {
     140  // A31, PC3_C
 };
 
+// void SystemClock_Config(void) {
+//   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+//   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+//   /** Supply configuration update enable
+//    */
+//   HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+
+//   /** Configure the main internal regulator output voltage
+//    */
+//   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+//   while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+//   }
+
+//   /** Initializes the RCC Oscillators according to the specified parameters
+//    * in the RCC_OscInitTypeDef structure.
+//    */
+//   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+//   RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
+//   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+//   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+//   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+//   RCC_OscInitStruct.PLL.PLLM = 4;
+//   RCC_OscInitStruct.PLL.PLLN = 60;
+//   RCC_OscInitStruct.PLL.PLLP = 2;
+//   RCC_OscInitStruct.PLL.PLLQ = 15;
+//   RCC_OscInitStruct.PLL.PLLR = 2;
+//   RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+//   RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+//   RCC_OscInitStruct.PLL.PLLFRACN = 0;
+//   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+//     Error_Handler();
+//   }
+
+//   /** Initializes the CPU, AHB and APB buses clocks
+//    */
+//   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
+//   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+//   RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+//   RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+//   RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+//   RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+//   RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+//   RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+
+//   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+//     Error_Handler();
+//   }
+
+//   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+//   // Configure PLL3 for 48 MHz USB
+//   PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+//   PeriphClkInit.PLL3.PLL3M = 32;  // PLL3 input divider (depends on your HSE)
+//   PeriphClkInit.PLL3.PLL3N = 192; // PLL3 multiplier
+//   PeriphClkInit.PLL3.PLL3P = 2;   // Optional for other peripherals
+//   PeriphClkInit.PLL3.PLL3Q = 8;   // USB divider
+//   PeriphClkInit.PLL3.PLL3R = 2;
+//   PeriphClkInit.PLL3.PLL3RGE = RCC_PLL3VCIRANGE_2; // input range
+//   PeriphClkInit.PLL3.PLL3VCOSEL = RCC_PLL3VCOWIDE;
+//   PeriphClkInit.PLL3.PLL3FRACN = 0;
+//   PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL3;
+
+//   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+//     Error_Handler();
+//   }
+// }
+
+
+// TODO: audit this
+// it should give 480 MHz core, 120MHz peripheral bus clock (APB), 48MHz USB clock, 120 MHz FDCAN peripheral clock
 void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
@@ -211,26 +283,28 @@ void SystemClock_Config(void) {
   /** Initializes the RCC Oscillators according to the specified parameters
    * in the RCC_OscInitTypeDef structure.
    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 60;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 120;
   RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 15;
+  RCC_OscInitStruct.PLL.PLLQ = 8;
   RCC_OscInitStruct.PLL.PLLR = 2;
   RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
   RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
   RCC_OscInitStruct.PLL.PLLFRACN = 0;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
    */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
@@ -239,7 +313,8 @@ void SystemClock_Config(void) {
   RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
   RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
     Error_Handler();
   }
 
@@ -247,17 +322,16 @@ void SystemClock_Config(void) {
 
   // Configure PLL3 for 48 MHz USB
   PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-  PeriphClkInit.PLL3.PLL3M = 32;  // PLL3 input divider (depends on your HSE)
-  PeriphClkInit.PLL3.PLL3N = 192; // PLL3 multiplier
-  PeriphClkInit.PLL3.PLL3P = 2;   // Optional for other peripherals
-  PeriphClkInit.PLL3.PLL3Q = 8;   // USB divider
+  PeriphClkInit.PLL3.PLL3M = 1;
+  PeriphClkInit.PLL3.PLL3N = 60;
+  PeriphClkInit.PLL3.PLL3P = 2;
+  PeriphClkInit.PLL3.PLL3Q = 10;
   PeriphClkInit.PLL3.PLL3R = 2;
-  PeriphClkInit.PLL3.PLL3RGE = RCC_PLL3VCIRANGE_2; // input range
-  PeriphClkInit.PLL3.PLL3VCOSEL = RCC_PLL3VCOWIDE;
+  PeriphClkInit.PLL3.PLL3RGE = RCC_PLL3VCIRANGE_3;
   PeriphClkInit.PLL3.PLL3FRACN = 0;
   PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL3;
-
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
     Error_Handler();
   }
 }
