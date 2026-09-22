9df8d371d56da2aa77ef5a4726cae6656e4c2404
jacobh460 <jacobhillman016@gmail.com>
2026-09-22 15:36:04 -0400

    remove HSE_VALUE from main

--- firmware/src/ec_main.cpp
+++ firmware/src/ec_main.cpp
@@ -29,9 +29,6 @@ bool kill_flag;
 bool arm_flag;
 void flight_loop();
 
-// jhillman added this to ensure HSE value is correct for all dynamic clock frequency calculations
-static_assert(HSE_VALUE == 8'000'000U);
-
 void setup() {
   // All shared interfaces are begun here.
 
